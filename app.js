3611443464466cd8b77c5b00a8225d64c6f46d66
adymoloca <ady.moloca@yahoo.com>
2019-09-06 18:11:39 +0300

    Call the loading function when document is loading

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const loader = document.querySelector('#loader');
 const container = document.querySelector('#container');
 
+// Create the show page function
 const showPage = () => {
     loader.style.display = 'none';
     container.style.display = 'block';
 }
 
+// Create the loading function and set the time for loader
 const loading = () => {
     setTimeout(showPage, 5000);
 }
 
+// Call the loading function when document is loading
 document.addEventListener('DOMContentLoaded', loading);
